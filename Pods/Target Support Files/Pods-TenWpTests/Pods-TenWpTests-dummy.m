abc6ee7acd883f74411a382913052dcc2cdb6afd
yaoquanfeng <yaoquanfeng@yoho8.com>
2017-04-14 15:23:23 +0800

    xxxx

---
+++ Pods/Target Support Files/Pods-TenWpTests/Pods-TenWpTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TenWpTests : NSObject
+@end
+@implementation PodsDummy_Pods_TenWpTests
+@end
---
+++ Pods/Target Support Files/Pods-TenWpUITests/Pods-TenWpUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TenWpUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TenWpUITests
+@end
