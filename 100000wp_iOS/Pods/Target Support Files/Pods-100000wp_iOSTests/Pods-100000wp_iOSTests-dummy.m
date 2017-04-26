d6185ef494bbddb941883d6d4635e3fbc2e636ae
yaoquanfeng <yaoquanfeng@yoho8.com>
2017-04-26 17:13:19 +0800

    remove all

--- 100000wp_iOS/Pods/Target Support Files/Pods-100000wp_iOSTests/Pods-100000wp_iOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_100000wp_iOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_100000wp_iOSTests
-@end
--- 100000wp_iOS/Pods/Target Support Files/Pods-100000wp_iOSUITests/Pods-100000wp_iOSUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_100000wp_iOSUITests : NSObject
-@end
-@implementation PodsDummy_Pods_100000wp_iOSUITests
-@end
