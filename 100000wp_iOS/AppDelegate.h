88f30e7110639d367fd33011e7ce1ec71c113603
yaoquanfeng <yaoquanfeng@yoho8.com>
2017-04-26 17:30:22 +0800

    no message

---
+++ 100000wp_iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  100000wp_iOS
+//
+//  Created by yoho8 on 2017/4/18.
+//  Copyright © 2017年 100000wp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pods/Target Support Files/Pods-100000wp_iOSTests/Pods-100000wp_iOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_100000wp_iOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_100000wp_iOSTests
+@end
