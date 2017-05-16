38f3cf9c91e0dd69219b382913e7e6e1d2ecaf18
tiantao <tiantao@lingyi365.com>
2017-05-16 13:41:52 +0800

    tts

--- 100000wp_iOS/AppDelegate.m
+++ 100000wp_iOS/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"10万+");
     return YES;
 }
 
